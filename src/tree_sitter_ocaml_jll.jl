b838582d3714d17e61f473b1a1a6bea741d9e97d
jlbuild <juliabuildbot@gmail.com>
2026-06-16 23:00:36 +0000

    tree_sitter_ocaml_jll build 0.25.0+0

--- src/tree_sitter_ocaml_jll.jl
+++ src/tree_sitter_ocaml_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("tree_sitter_ocaml")
-JLLWrappers.@generate_main_file("tree_sitter_ocaml", UUID("58c1feab-41fe-5716-9e27-932daa5b6f35"))
+JLLWrappers.@generate_main_file("tree_sitter_ocaml", Base.UUID("58c1feab-41fe-5716-9e27-932daa5b6f35"))
 end  # module tree_sitter_ocaml_jll
